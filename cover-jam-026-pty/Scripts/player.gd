38f3956d41f4f36730da27729befb03935ab2a8d
lucas-de-paulo <lucasdepaulo.bernardo@gmail.com>
2026-05-30 20:10:38 -0300

    A

--- cover-jam-026-pty/Scripts/player.gd
+++ cover-jam-026-pty/Scripts/player.gd
@@ -6,6 +6,8 @@ var health = 100
 var level = 0
 var xp = 0
 
+var range = preload("res://range.tscn").instantiate()
+
 func Check_xp():
 	if xp >= Global.current_levelup:
 		Global.current_levelup = Global.current_levelup * 2.10
@@ -29,9 +31,11 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.y = move_toward(velocity.y, 0, speed)
 	move_and_slide()
-
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		add_child(range)
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_name == "Enemy":
 		Damage(body.damage)
-	if body.is_name == "xp":
-		
