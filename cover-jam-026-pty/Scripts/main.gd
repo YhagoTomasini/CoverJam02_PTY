f37eb0a5aaf1c09c08a8355f7cef2506626c2cc0
Artêmio Neto <artemiorabello123@gmail.com>
2026-05-31 15:30:44 -0300

    Camera melhorada

--- cover-jam-026-pty/Scripts/main.gd
+++ cover-jam-026-pty/Scripts/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = %Player
-@onready var morte: Control = $CanvasLayer/Morte
+@onready var morte: Control = %Morte
 
 var player_morreu : bool
 
