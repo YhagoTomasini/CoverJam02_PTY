8086c62d19f8cb896864783f3d67aa03dfff5178
Artêmio Neto <artemiorabello123@gmail.com>
2026-05-30 21:25:39 -0300

    Upgrades arma incabado

--- cover-jam-026-pty/Scripts/scene_upgrade.gd
+++ cover-jam-026-pty/Scripts/scene_upgrade.gd
@@ -5,44 +5,44 @@ extends Control
 @onready var button_3: Button = $Button3
 
 
-var SPEED_icon = preload("uid://cfpke015c26qd")
-var SPEED_text = "A Train:
-	Aumenta 5% a velocidade do progenitor do caos"
+var macunaima_icon = preload("uid://cfpke015c26qd")
+var macunaima_text = "Macunaima"
 
-var HP_icon = preload("uid://cfpke015c26qd")
-var HP_text = "VIDA:
-	Recupera 20% da vida
-	Aumenta a vida maxima em 10%"
+var mula_icon = preload("uid://cfpke015c26qd")
+var mula_text = "Mula sem cabeça"
 
-var XP_icon = preload("uid://cfpke015c26qd")
-var XP_text = "Mais sangue:
-Aumenta em 15% o ganho de sangue dos seus inimigos"
+var corpo_icon = preload("uid://cfpke015c26qd")
+var corpo_text = "Corpo seco"
 
-var DANO_icon = preload("uid://cfpke015c26qd")
-var DANO_text = "O criador do caos:
-	Aumenta o dano de todas as suas armas + 10%"
+var pistola_icon = preload("uid://cfpke015c26qd")
+var pistola_text = "Pistola"
 	
-var CD_icon = preload("uid://cfpke015c26qd")
-var CD_text = "MORRA MORRA MORA:
-	Diminui o tempo de recarga das habilidades + 10%"
+var bazuka_icon = preload("uid://cfpke015c26qd")
+var bazuka_text = "Bazuka"
 	
-var SIZE_icon = preload("uid://cfpke015c26qd")
-var SIZE_text = "Ninguem escapara:
-	Aumenta o tamanho de seus projeteis + 10%"
+var faca_icon = preload("uid://cfpke015c26qd")
+var faca_text = "Faca"
 
 
-var HP = [HP_icon, HP_text,0.25]
-var SPEED = [SPEED_icon, SPEED_text,0.05]
-var XP = [XP_icon, XP_text, 0.15]
-var DANO = [DANO_icon,DANO_text, 0.1]
-var CD = [CD_icon, CD_text, 0.1]
-var SIZE = [SIZE_icon, SIZE_text, 0.1]
+var macunaima = [macunaima_icon, macunaima_text,0.25]
+var mula = [mula_icon, mula_text,0.05]
+var corpo = [corpo_icon, corpo_text, 0.15]
+var pistola = [pistola_icon,pistola_text, 0.1]
+var bazuka = [bazuka_icon, bazuka_text, 0.1]
+var faca = [faca_icon, faca_text, 0.1]
 
-var arrayDePowerUps =[HP,SPEED,XP,DANO,CD,SIZE] 
+var arrayDeArmas =[faca,bazuka,pistola,corpo,mula,macunaima] 
 
 var powerUp1 
 var powerUp2
 var powerUp3
+
+var faca_ativa = false
+var bazuka_ativa = false
+var pistola_ativa = false
+var corpo_ativa = false
+var mula_ativa = false
+var macunaima_ativa = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -55,113 +55,121 @@ func _process(_delta: float) -> void:
 		
 func LeveUp():
 	visible = true
-	powerUp1 = arrayDePowerUps.pick_random()
-	powerUp2 = arrayDePowerUps.pick_random()
-	powerUp3 = arrayDePowerUps.pick_random()
+	powerUp1 = arrayDeArmas.pick_random()
+	powerUp2 = arrayDeArmas.pick_random()
+	powerUp3 = arrayDeArmas.pick_random()
+	if faca_ativa or bazuka_ativa or pistola_ativa or corpo_ativa or mula_ativa or macunaima_ativa:
+		powerUp1 = arrayDeArmas.pick_random()
+	if faca_ativa or bazuka_ativa or pistola_ativa or corpo_ativa or mula_ativa or macunaima_ativa:
+		powerUp2 = arrayDeArmas.pick_random()
+	if faca_ativa or bazuka_ativa or pistola_ativa or corpo_ativa or mula_ativa or macunaima_ativa:
+		powerUp3 = arrayDeArmas.pick_random()
+
 	match powerUp1:
-		HP:
-			button.text = HP[1]
-			button.icon = HP[0]
-		SPEED:
-			button.text = SPEED[1]
-			button.icon = SPEED[0]
-		XP:
-			button.text = XP[1]
-			button.icon = XP[0]
-		DANO:
-			button.text = DANO[1]
-			button.icon = DANO[0]
-		CD:
-			button.text = CD[1]
-			button.icon = CD[0]
-		SIZE:
-			button.text = SIZE[1]
-			button.icon = SIZE[0]
+		faca:
+			button.text = faca[1]
+			button.icon = faca[0]
+		bazuka:
+			button.text = bazuka[1]
+			button.icon = bazuka[0]
+		pistola:
+			button.text = pistola[1]
+			button.icon = pistola[0]
+		corpo:
+			button.text = corpo[1]
+			button.icon = corpo[0]
+		mula:
+			button.text = mula[1]
+			button.icon = mula[0]
+		macunaima:
+			button.text = macunaima[1]
+			button.icon = macunaima[0]
 	match powerUp2:
-		HP:
-			button_2.text = HP[1]
-			button_2.icon = HP[0]
-		SPEED:
-			button_2.text = SPEED[1]
-			button_2.icon = SPEED[0]
-		XP:
-			button_2.text = XP[1]
-			button_2.icon = XP[0]
-		DANO:
-			button_2.text = DANO[1]
-			button_2.icon = DANO[0]
-		CD:
-			button_2.text = CD[1]
-			button_2.icon = CD[0]
-		SIZE:
-			button_2.text = SIZE[1]
-			button_2.icon = SIZE[0]
+		faca:
+			button_2.text = faca[1]
+			button_2.icon = faca[0]
+		bazuka:
+			button_2.text = bazuka[1]
+			button_2.icon = bazuka[0]
+		pistola:
+			button_2.text = pistola[1]
+			button_2.icon = pistola[0]
+		corpo:
+			button_2.text = corpo[1]
+			button_2.icon = corpo[0]
+		mula:
+			button_2.text = mula[1]
+			button_2.icon = mula[0]
+		macunaima:
+			button_2.text = macunaima[1]
+			button_2.icon = macunaima[0]
 	match powerUp3:
-		HP:
-			button_3.text = HP[1]
-			button_3.icon = HP[0]
-		SPEED:
-			button_3.text = SPEED[1]
-			button_3.icon = SPEED[0]
-		XP:
-			button_3.text = XP[1]
-			button_3.icon = XP[0]
-		DANO:
-			button_3.text = DANO[1]
-			button_3.icon = DANO[0]
-		CD:
-			button_3.text = CD[1]
-			button_3.icon = CD[0]
-		SIZE:
-			button_3.text = SIZE[1]
-			button_3.icon = SIZE[0]
+		faca:
+			button_3.text = faca[1]
+			button_3.icon = faca[0]
+		bazuka:
+			button_3.text = bazuka[1]
+			button_3.icon = bazuka[0]
+		pistola:
+			button_3.text = pistola[1]
+			button_3.icon = pistola[0]
+		corpo:
+			button_3.text = corpo[1]
+			button_3.icon = corpo[0]
+		mula:
+			button_3.text = mula[1]
+			button_3.icon = mula[0]
+		macunaima:
+			button_3.text = macunaima[1]
+			button_3.icon = macunaima[0]
 
 func _on_button_pressed() -> void:
 	match powerUp1:
-		HP:
-			Global.Multiple_Health += HP[2]
-		SPEED:
-			Global.Multiple_Speed += SPEED[2]
-		XP:
-			Global.Multiple_xp += XP[2]
-		DANO:
-			Global.Multiple_Damage += DANO[2]
-		CD:
-			Global.Multiple_CD += DANO[2]
-		SIZE:
-			Global.Multiple_size += DANO[2]
+		faca:
+			faca_ativa = true
+			print(faca)
+		bazuka:
+			pass
+		pistola:
+			pass
+		corpo:
+			pass
+		mula:
+			pass
+		macunaima:
+			pass
 	visible = false
 
 
 func _on_button_2_pressed() -> void:
 	match powerUp2:
-		HP:
-			Global.Multiple_Health += HP[2]
-		SPEED:
-			Global.Multiple_Speed += SPEED[2]
-		XP:
-			Global.Multiple_xp += XP[2]
-		DANO:
-			Global.Multiple_Damage += DANO[2]
-		CD:
-			Global.Multiple_CD += DANO[2]
-		SIZE:
-			Global.Multiple_size += DANO[2]
+		faca:
+			pass
+		bazuka:
+			pass
+		pistola:
+			pass
+		corpo:
+			pass
+		mula:
+			pass
+		macunaima:
+			pass
 	visible = false
 
 
 func _on_button_3_pressed() -> void:
 	match powerUp3:
-		HP:
-			Global.Multiple_Health += HP[2]
-		SPEED:
-			Global.Multiple_Speed += SPEED[2]
-		XP:
-			Global.Multiple_xp += XP[2]
-		DANO:
-			Global.Multiple_Damage += DANO[2]
-		CD:
-			Global.Multiple_CD += DANO[2]
-		SIZE:
-			Global.Multiple_size += DANO[2]
+		faca:
+			pass
+		bazuka:
+			pass
+		pistola:
+			pass
+		corpo:
+			pass
+		mula:
+			pass
+		macunaima:
+			pass
 	visible = false
