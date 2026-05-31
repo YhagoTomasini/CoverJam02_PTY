3514b75891d2749f2160b030d8952680bd5b3b1d
Artêmio Neto <artemiorabello123@gmail.com>
2026-05-31 16:38:11 -0300

    Florezinhas para todes
    Spawn das flores + flores iniciais

--- cover-jam-026-pty/Scripts/main.gd
+++ cover-jam-026-pty/Scripts/main.gd
@@ -2,11 +2,22 @@ extends Node2D
 
 @onready var player: CharacterBody2D = %Player
 @onready var morte: Control = %Morte
+const FLOWER = preload("uid://cvn3rdjlp7fhi")
+
 
 var player_morreu : bool
+
+func florecendo():
+	var spawn_flor = FLOWER.instantiate()
+	spawn_flor.position = Vector2(randi_range(-640,1280),randi_range(-480,960))
+	add_sibling(spawn_flor)
 
 func gameover():
 	player_morreu = true
 	print("morreu")
 	morte.visible = true
 	get_tree().paused = true
+
+
+func _on_spawner_de_flor_timeout() -> void:
+	florecendo()
